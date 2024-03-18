8a189cd5be1d499ebbb50c58c4e2f6b68f5ff71f
OrlandoBussolo <orlando_linux@DESKTOP-ISNUI81>
2024-03-18 17:06:08 -0300

    add extract and save

---
+++ scripts/Extract_football_data_to_mongodb.py
@@ -0,0 +1,57 @@
+import os
+import requests
+import pandas as pd
+from datetime import date, timedelta
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+def connect_mongo(uri):    
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    return client    
+
+def create_connect_db(client, db_name):
+    db = client[db_name]
+    return db
+
+def create_connect_collection(db, col_name):
+    collection_game = db[col_name]
+    return collection_game
+
+
+def extract_api_data():    
+    league_id = 152 #PremiereLeague
+    api_key = "39d6200996e3a32617a5c7c4cf4fdafc309a3abe279b307c1eecb31a43c41a74"    
+    today = date.today()
+    # Extract data from seven days ago up to today
+    week_ago = today - timedelta(days=50)
+   
+    url_base = f"https://apiv3.apifootball.com/?action=get_events&from={week_ago}&to={today}&league_id={league_id}&APIkey={api_key}"
+    response = requests.get(url_base).json()
+    return response
+    
+def insert_data(col, data):
+    result = col.insert_many(data)
+    n_docs_inseridos = len(result.inserted_ids)
+    return n_docs_inseridos
+
+if __name__ == "__main__":
+    client = connect_mongo("mongodb+srv://orlandobussolo:asdfg...@cluster-pipeline.pmkfr1g.mongodb.net/?retryWrites=true&w=majority&appName=cluster-pipeline")
+    db =  create_connect_db(client, "db_games")
+    col = create_connect_collection(db, "premiere_league")
+
+    data = extract_api_data()
+    print(f"\nTotal Amount of data extracted: {len(data)}")
+
+    n_docs = insert_data(col, data)
+    print(f"\nTotal amount of data inserted: {n_docs}")
+    #close connection
+    client.close()
