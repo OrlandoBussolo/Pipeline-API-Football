53939abfe90e5d99bbc7967436d648d3bed14034
OrlandoBussolo <orlando_linux@DESKTOP-ISNUI81>
2024-03-18 17:16:05 -0300

    add all data from local to remote

---
+++ scripts/Transform_data_to_csv.py
@@ -0,0 +1,46 @@
+from extract_and_save_data import connect_mongo, create_connect_db, create_connect_collection
+import pandas as pd
+import os
+
+def visualize_collection(col):
+    for doc in col.find():
+        print(doc)
+
+def create_dataframe(col):
+    list_games = []
+    for c in col.find():
+        list_games.append(c)
+    df_games = pd.DataFrame(list_games)    
+    return df_games  
+
+def filter_dataframe(df):
+    columns = ['match_id', 'league_id', 'match_date', 'match_time', 'match_hometeam_id',
+               'match_hometeam_name', 'match_hometeam_score', 'match_awayteam_name',
+               'match_awayteam_id', 'match_awayteam_score', 'match_round', 'match_stadium',
+               'match_referee','league_year']
+
+    # Remove the columns
+    df_games_filtered = df[columns]
+    return df_games_filtered
+
+def save_csv(df, path):
+    # Verifica se o diretório de saída existe, caso contrário, cria
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    df.to_csv(path, index=False)
+    print(f"\nThe file {path} is safe")
+
+if __name__ == "__main__":
+    # estabelecendo a conexão e recuperando os dados do MongoDB
+    client = connect_mongo("mongodb+srv://orlandobussolo:asdfg123@cluster-pipeline.pmkfr1g.mongodb.net/?retryWrites=true&w=majority&appName=cluster-pipeline")
+    db =  create_connect_db(client, "db_games")
+    col = create_connect_collection(db, "premiere_league")
+
+    df_games = create_dataframe(col)
+    df_games_filtered = filter_dataframe(df_games )
+    save_csv(df_games_filtered, "/home/orlando_linux/pipeline-python-mongo-mysql/data_teste/tabela.csv")
+
+   
+
+    
---
+++ scripts/to_mysql.py
@@ -0,0 +1,87 @@
+import mysql.connector
+import pandas as pd
+import numpy as np
+
+def connect_mysql(host_name, user_name, pw):
+    cnx = mysql.connector.connect(
+        host = host_name,
+        user = user_name,
+        password = pw
+    )
+    print(cnx)
+    return cnx
+
+def create_cursor(cnx):
+    cursor = cnx.cursor()
+    return cursor
+
+def create_database(cursor, db_name):
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+    print(f"\nBase de dados {db_name} criada")
+
+def show_databases(cursor):
+    cursor.execute("SHOW DATABASES")
+    for x in cursor:
+        print(x)
+
+def create_championship_table(cursor, db_name, tb_name):    
+    cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS {db_name}.{tb_name}(
+        
+        match_id INT,
+        league_id INT,
+        match_date DATE,
+        match_time TIME,
+        match_hometeam_id INT,
+        match_hometeam_name VARCHAR(255),
+        match_hometeam_score INT,
+        match_awayteam_name VARCHAR(255),
+        match_awayteam_id INT,
+        match_awayteam_score INT,
+        match_round INT,
+        match_stadium VARCHAR(255),
+        match_referee VARCHAR(255),
+        league_year VARCHAR(50),
+                
+        PRIMARY KEY (match_id)
+    )""")
+                   
+    print(f"\nTabela {tb_name} criada")
+
+def show_tables(cursor, db_name):
+    cursor.execute(f"USE {db_name}")
+    cursor.execute("SHOW TABLES")
+    for x in cursor:
+        print(x)
+
+def read_csv(path):
+    df = pd.read_csv(path)
+    return df
+
+def add_data(cnx, cursor, df, db_name, tb_name):
+    lista = [tuple(row) for i, row in df.iterrows()]
+    lista = [tuple(None if pd.isna(item) else item for item in tupla) for tupla in lista]
+    sql = f"INSERT INTO {db_name}.{tb_name} VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+    cursor.executemany(sql, lista)
+    print(f"\n {cursor.rowcount} dados foram inseridos na tabela {tb_name}.")
+    cnx.commit()
+
+if __name__ == "__main__":
+    
+    # realizando a conexão com mysql
+    cnx = connect_mysql("localhost", "orlando_bussolo", "asdfg...")
+    cursor = create_cursor(cnx)
+
+    # criando a base de dados
+    create_database(cursor, "db_games")
+    show_databases(cursor)
+
+    # criando tabela
+    create_championship_table(cursor, "db_games", "premiere_league")
+    show_tables(cursor, "db_games")
+
+    # lendo e adicionando os dados
+    df = read_csv("/home/orlando_linux/pipeline-python-mongo-mysql/data_teste/tabela.csv")
+    add_data(cnx, cursor, df, "db_games", "premiere_league")
+
